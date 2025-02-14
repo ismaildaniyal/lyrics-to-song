78037f4b86c85352799a42a74e7f57b39a696a9f
IsmailDanial <158290076+ismaildaniyal@users.noreply.github.com>
2025-02-14 19:22:13 +0500

    Update App.py

--- App.py
+++ App.py
@@ -8,7 +8,7 @@ from gtts import gTTS
 
 # Configure API Key
 load_dotenv()
-GEMINI_API_KEY = os.getenv("gemini")
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 
 # Load Whisper AI (Speech-to-Text)
 model = whisper.load_model("small")
